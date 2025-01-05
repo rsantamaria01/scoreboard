9e6f4d0249f7060af75ea7db4b9c2bddf6717aa6
Raul Santamaria <raulsantamariao@hotmail.com>
2025-01-05 15:04:18 -0500

    Refactor main entry points to implement microservices for users and games, updating transport configurations and logging

--- backend/games/src/main.ts
+++ backend/games/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('GAMES');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3001,
+      },
+    }
+  );
+  await app.listen();
+  logger.log('Microservice is active on port 3001');
 }
 bootstrap();
--- backend/users/src/main.ts
+++ backend/users/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('USERS');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3002,
+      },
+    }
+  );
+  await app.listen();
+  logger.log('Microservice is active on port 3002');
 }
 bootstrap();
