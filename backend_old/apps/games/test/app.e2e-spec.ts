3a3bb7d1bf1cea2e3ea6c2e10cf53ed4a32ce95a
Raul Santamaria <raulsantamariao@hotmail.com>
2025-01-05 13:57:05 -0500

    Changes to be committed: 	modified:   backend_old/apps/backend/test/app.e2e-spec.ts 	modified:   backend_old/apps/games/test/app.e2e-spec.ts 	modified:   backend_old/apps/getway/test/app.e2e-spec.ts 	modified:   backend_old/apps/users/test/app.e2e-spec.ts

--- backend_old/apps/games/test/app.e2e-spec.ts
+++ backend_old/apps/games/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { GamesModule } from './../src/games.module';
+import { GamesModule } from '../src/games.module';
 
 describe('GamesController (e2e)', () => {
   let app: INestApplication;
--- backend_old/apps/getway/test/app.e2e-spec.ts
+++ backend_old/apps/getway/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { GetwayModule } from './../src/getway.module';
+import { GetwayModule } from '../src/getway.module';
 
 describe('GetwayController (e2e)', () => {
   let app: INestApplication;
