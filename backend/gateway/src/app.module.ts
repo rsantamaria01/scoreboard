8a8b57c4eddec2310f5e2ad85e79fde18cb9e4ea
Raul Santamaria <raulsantamariao@hotmail.com>
2025-01-05 19:17:33 -0500

    Refactor project structure by removing old files and adding new DTOs and configuration for users

--- backend/gateway/src/app.module.ts
+++ backend/gateway/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GamesModule } from './games/games.module';
+import { UsersModule } from './users/users.module';
 import { GamesModule } from './games/games.module';
 
 @Module({
-  imports: [GamesModule],
+  imports: [GamesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
